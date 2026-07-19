6facdb40a27b9ec5b1b9e431edbcd7d62d798c94
sahunaincy <naincys2004@gmail.com>
2026-07-19 07:49:20 +0000

    comparision

---
+++ 01_basic/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log(2==2)//same datatype hai to ye predicted output he dege 
+// console.log(2>=2)
+// console.log(2<=2)
+// console.log(2<3)
+
+// console.log("2">1)//ye different datatype hai to ye possible hai ki predicted output na de
+// console.log("naincy"==false)
+
+// console.log(null>0);//ye comparisons hai to isme ye null ko number mai convert kar dega 
+// console.log(null==0);//isme equality check ho raha hai 
+// console.log(null>=0);
+
+// console.log(undefined=="naincy");
+// console.log(undefined==0);//return false always for different datatype
+
+//=== ye strick check hai isme ye value kai sath sath datatype bhi check karte hai
+console.log("2"==2);//its give true because it check only value
+console.log("2"===2);//it give false because it check value asa well as datatype
